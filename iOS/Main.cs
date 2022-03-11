5b75fe18bdc624e349b366caa01bc680fcc71afa
Michael Stonis <michael.stonis@eightbot.com>
2022-03-11 08:54:44 -0600

    updating packages and projects

--- iOS/Main.cs
+++ iOS/Main.cs
@@ -14,7 +14,7 @@ namespace ReactiveExtensionExamples.iOS
 		{
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
-			UIApplication.Main (args, null, "AppDelegate");
+			UIApplication.Main (args, null, typeof(AppDelegate));
 		}
 	}
 }
